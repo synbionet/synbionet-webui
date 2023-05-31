ad79ba37b0c822b77dd03786e8a00a20a5b343fa
Patrick O'Leary <poleary@mitre.org>
2023-05-31 18:30:00 -0400

    clean up explore view

--- src/components/ExploreViewBody.jsx
+++ src/components/ExploreViewBody.jsx
@@ -1,42 +1,31 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { BioAssetCard } from './BioAssetCard'
 import { useEffect } from 'react'
-import { setBioAssets } from '../store/accountStore'
 import { Link } from 'react-router-dom'
-import { fetchAssets } from '../utils'
 import { GridLoader } from './common/GridLoader'
 import { useState } from 'react'
 
 export function ExploreViewBody() {
-  const dispatch = useDispatch()
-  const bioAssets = useSelector((state) => state.account.bioAssets)
   const [isLoading, setIsLoading] = useState(false)
-
-  // TODO: fix this temp filter to show what assets are currently listed on market. May not be correct since licenses can run out.
-  // const marketAssets = bioAssets.filter((asset) => asset.availableLicenses > 0)
-  const serviceProviders = bioAssets.filter((asset) => asset.assetType === 'provider')
 
   useEffect(() => {
     async function getAssets() {
       setIsLoading(true)
-      dispatch(setBioAssets(await fetchAssets()))
       setIsLoading(false)
     }
     getAssets()
-  }, [dispatch])
+  })
 
   return (
     <div className="flex flex-wrap mx-8">
-      {serviceProviders.map((asset) => {
+      {[1, 2, 3, 4].map((asset) => {
         return (
-          <div key={asset.did} className="w-1/4 p-3">
+          <div key={asset} className="w-1/4 p-3">
             <Link to={`/asset/${asset.did}`}>
-              <BioAssetCard marketView asset={asset} />
+              <div className="bg-gray-400 h-64"></div>
             </Link>
           </div>
         )
       })}
-      {serviceProviders.length === 0 && isLoading && <GridLoader />}
+      {isLoading && <GridLoader />}
     </div>
   )
 }
