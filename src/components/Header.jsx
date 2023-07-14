df2b5e68380fbdc508311fc2bb715efc1d069440
Patrick O'Leary <poleary@mitre.org>
2023-07-14 16:49:15 -0400

    update navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,20 @@
-import { HeaderButton } from './HeaderButton'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import { ConnectKitButton } from 'connectkit'
+import Box from '@mui/material/Box'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
 
-function NavBar({ connectWallet }) {
-  const location = useLocation()
-
+function NavBar() {
   return (
-    <div className="flex items-center space-x-10">
-      <div className="flex space-x-14">
-        <Link to="/network">
-          <HeaderButton buttonTitle="Network" isActive={location.pathname === '/network'} />
-        </Link>
-        <Link to="/portfolio">
-          <HeaderButton buttonTitle="Portfolio" isActive={location.pathname === '/portfolio'} />
-        </Link>
-      </div>
+    <div className="flex items-center">
+      <NavTabs />
       <ConnectKitButton />
     </div>
   )
 }
 
-export function Header({ connectWallet }) {
+export function Header() {
   return (
     <header className="flex items-center justify-between pl-4 pr-8 h-16 bg-slate-100 border border-slate-300">
       <Link to="/">
@@ -34,7 +27,33 @@ export function Header({ connectWallet }) {
           </div>
         </div>
       </Link>
-      <NavBar connectWallet={connectWallet} />
+      <NavBar />
     </header>
+  )
+}
+
+function LinkTab(props) {
+  return (
+    <Link to={props.href}>
+      <Tab disableRipple {...props} />
+    </Link>
+  )
+}
+
+function NavTabs() {
+  const location = useLocation()
+  const pages = {
+    network: 0,
+    portfolio: 1,
+  }
+  const path = `${location.pathname.substring(1)}`
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Tabs value={path in pages ? pages[path] : -1} aria-label="nav tabs">
+        <LinkTab label="Network" href="/network" />
+        <LinkTab label="Portfolio" href="/portfolio" />
+      </Tabs>
+    </Box>
   )
 }
